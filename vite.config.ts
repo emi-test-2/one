7fab5dfb5585d2017e56c4ddb9042bdcb14737b5
329439E932 <193024651+329439E932@users.noreply.github.com>
2025-02-07 22:13:00 -0300

    Agregar base de configuración en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "329439E932/one",
   plugins: [
     vue(),
     tailwindcss(),
