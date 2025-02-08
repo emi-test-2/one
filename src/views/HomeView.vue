fd83c2488c4b78d48caae5b37b3fce5a42ce0bb6
329439E932 <193024651+329439E932@users.noreply.github.com>
2025-02-07 22:04:25 -0300

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="flex justify-center items-center w-full h-screen">
+    <div>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+        Boton
+      </button>
+    </div>
+  </main>
+</template>
