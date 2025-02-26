8c89c193be6a19c3c102dbeb14875e6b810ad6e8
emi-test-2 <193024651+emi-test-2@users.noreply.github.com>
2025-02-26 20:32:17 -0300

    Agregar saludo "Hola mundo" en HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
 
 <template>
   <main class="flex justify-center items-center w-full h-screen">
+    Hola mundo
     <div>
       <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
         Boton
