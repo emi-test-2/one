357371b16a35776363fa7d4869120d659c536051
emi-test-2 <193024651+emi-test-2@users.noreply.github.com>
2025-02-27 22:01:15 -0300

    Agregar imagen y ajustar estilo del botón en HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+const baseUrl = import.meta.env.BASE_URL;
+
+
 </script>
 
 <template>
   <main class="flex justify-center items-center w-full h-screen">
-    Hola mundo
     <div>
-      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer mx-auto block">
         Boton
       </button>
+      <img class="mt-5" :src="`${baseUrl}/pup.jpg`"/>
     </div>
   </main>
 </template>
